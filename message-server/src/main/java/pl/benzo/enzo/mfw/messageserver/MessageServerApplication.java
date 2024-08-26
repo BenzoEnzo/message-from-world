a2e87c9caf4cfec1e9846a3bd6d81713977bac32
BenzoEnzo <goxer@o2.pl>
2024-08-26 23:59:20 +0200

    init

---
+++ message-server/src/main/java/pl/benzo/enzo/mfw/messageserver/MessageServerApplication.java
@@ -0,0 +1,13 @@
+package pl.benzo.enzo.mfw.messageserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageServerApplication.class, args);
+    }
+
+}
